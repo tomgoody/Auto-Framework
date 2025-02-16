e90b41e11274a28401ade08d307a7dce507b8d5c
tomgoody <tgoody1986@gmail.com>
2025-02-16 09:54:28 -0700

    completing test

---
+++ TestCases/AddContractTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using NvendorPOM.PageMethod;
+using OpenQA.Selenium;
+using NvendorPOM.Config;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+using AventStack.ExtentReports;
+using NvendorPOM.PageMethod.Nvendor;
+//using DescriptionAttribute = NUnit.Framework.DescriptionAttribute;
+
+namespace NvendorPOM.TestCases
+{
+    [TestFixture]
+    public class AddContractTest : Configuration
+    {
+
+        LoginPage loginPage;
+        ContractManager contractManager;
+
+
+        // The login could be cleaned up so that on a new test, all that needs to be called is one loginPage.login()
+        [SetUp]
+        public void Test_login()
+        {
+            loginPage = new LoginPage(GetDriver());
+            loginPage.goToPage();
+            loginPage.enterUserName();
+            loginPage.enterPassword();
+            loginPage.selectInstitution();
+            loginPage.selectProduct();
+            loginPage.verifyProduct();
+        }
+
+        [Test, Description("Add a Contract"), Order(1)]
+        [Category("Add Contract")]
+        public void Test_addContract()
+        {
+            contractManager = new ContractManager(GetDriver());
+            contractManager.goToContracts();
+            contractManager.uploadContract();
+            contractManager.changeContractName();
+            contractManager.setAgreementType();
+            contractManager.setVendor();
+            //contractManager.setContractType();
+            contractManager.nextSaveButtons();
+            contractManager.searchContractUpload();
+            contractManager.VerifyContractUpload();
+           //var results = contractManager.VerifyContractUpload();
+            //Assert.That(results, Is.EqualTo(true));
+        }
+
+    }
+}
---
+++ TestCases/EditContractTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using NvendorPOM.PageMethod;
+using OpenQA.Selenium;
+using NvendorPOM.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+using NvendorPOM.PageMethod.Nvendor;
+
+
+namespace NvendorPOM.TestCases
+{
+    [TestFixture]
+    internal class EditContractTests : Configuration
+    {
+
+        LoginPage loginPage;
+        ContractManager contractManager;
+
+
+        [SetUp]
+        public void Test_login()
+        {
+            loginPage = new LoginPage(GetDriver());
+            loginPage.goToPage();
+            loginPage.enterUserName();
+            loginPage.enterPassword();
+            loginPage.selectInstitution();
+            loginPage.selectProduct();
+            loginPage.verifyProduct();
+        }
+
+        [Test, Category("Edit Contract"), Order(2)]
+        public void Test_EditContractName()
+        {
+            contractManager = new ContractManager(GetDriver());
+            contractManager.goToContracts();
+            contractManager.EditContractName();
+            contractManager.verifyContractNameEdit();
+        }
+
+        //[Test, Category("Edit Contract"), Order(3)]
+        //public void Test_EditContractDescription()
+        //{
+        //    contractManager = new ContractManager(GetDriver());
+        //   var results = contractManager.goToAIPanel();
+        //    if (results == true)
+        //    {
+        //        Assert.Pass("We did it!");
+        //    }
+        //}
+    }
+}
